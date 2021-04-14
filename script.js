d2c97a2cb5b4d7a3a2f25df74d5bd6f9d7b233e8
Jimmy <zhan0662@algonquinlive.com>
2021-04-14 14:33:54 -0400

    assignment4

--- script.js
+++ script.js
@@ -13,28 +13,28 @@ hamburger.addEventListener('click', function() {
 // First article
 const $home = document.getElementById('home')
 const $id1 = document.getElementById('1')
-$home.addEventListener('click', function(){
+window.addEventListener('scroll', function(){
   $id1.classList.toggle("is-visible")
 })
 
 // Second article
 const $portfolio = document.getElementById('portfolio')
 const $id2 = document.getElementById('2')
-$portfolio.addEventListener('click', function(){
+window.addEventListener('scroll', function(){
   $id2.classList.toggle("is-visible")
 })
 
 // Third article
 const $images = document.getElementById('images')
 const $id3 = document.getElementById('3')
-$images.addEventListener('click', function(){
+window.addEventListener('scroll', function(){
   $id3.classList.toggle("is-visible")
 })
 
 // Fourth article
 const $contact = document.getElementById('contact')
 const $id4 = document.getElementById('4')
-$contact.addEventListener('click', function(){
+window.addEventListener('scroll', function(){
   $id4.classList.toggle("is-visible")
 })
 
